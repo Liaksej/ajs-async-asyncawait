982291676b8aaeda0912bf78aee9a056dc3a3e5c
Liaksej <tarusov.liaksej@gmail.com>
2023-06-20 20:12:54 +0200

    Add class GameSaving.js for make obj

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,9 +1,10 @@
 import json from "./parser.js";
 import read from "./reader.js";
+import { GameSaving } from "./GameSaving.js";
 
 export class GameSavingLoader {
   static async load() {
     const readed = await read();
-    return await json(readed);
+    return new GameSaving(JSON.parse(await json(readed)));
   }
 }
